33cfa38c2e7171e8a12cf3196feb5b885278913a
zakovyrya <zakovyrya@b74d255b-db50-0410-b75c-658c0ede0284>
2008-07-09 05:55:48 +0000

    Minor UI enhancements

--- ArCHMock/current/CHMApplicationDelegate.m
+++ ArCHMock/current/CHMApplicationDelegate.m
@@ -1,18 +1,23 @@
 #import "CHMApplicationDelegate.h"
 #import "CHMURLProtocol.h"
+#import "CHMDocumentController.h"
 
 @implementation CHMApplicationDelegate
 
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)application {
+    return YES;
+}
+
 - (void)applicationWillFinishLaunching:(NSNotification *)notification {
     [NSURLProtocol registerClass:[CHMURLProtocol class]];
+
+    [[CHMDocumentController sharedCHMDocumentController] loadBookmarks:self];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)notification {
     [NSURLProtocol unregisterClass:[CHMURLProtocol class]];
-}
-
-- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication {
-    return YES;
+    
+    [[CHMDocumentController sharedCHMDocumentController] saveBookmarks:self];
 }
 
 @end
--- ArCHMock/current/CHMDocumentController.h
+++ ArCHMock/current/CHMDocumentController.h
@@ -2,13 +2,26 @@
 
 
 @interface CHMDocumentController : NSDocumentController {
-    NSMutableDictionary *documentByContainerID;
+    NSMutableDictionary *loadedDocumentByContainerID;
     NSOperationQueue *operationQueue;
+    
+    NSString *applicationSupportFolderPath;
+    
+    NSMutableArray *bookmarks;
+    NSString *bookmarksFilePath;
 }
 
-@property (retain) NSMutableDictionary *documentByContainerID;
+@property (retain) NSMutableDictionary *loadedDocumentByContainerID;
 @property (retain) NSOperationQueue *operationQueue;
 
+@property (retain) NSMutableArray *bookmarks;
+@property (retain) NSString *bookmarksFilePath;
+@property (retain) NSString *applicationSupportFolderPath;
+
 + (CHMDocumentController *)sharedCHMDocumentController;
+
+- (IBAction)loadBookmarks:(id)sender;
+- (IBAction)editBookmarks:(id)sender;
+- (IBAction)saveBookmarks:(id)sender;
 
 @end
